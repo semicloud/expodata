347c014a8548d65e14bf188e48494e015232ee55
semicloud <semicloud@163.com>
2019-05-27 16:50:56 +0800

    Basic servlet environment established

---
+++ src/main/java/cn/iscas/PredictNowCountTotalCount.java
@@ -0,0 +1,22 @@
+package cn.iscas;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PredictNowCountTotalCount extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.println("Ok");
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
