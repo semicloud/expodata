0dabcade1d83ec3d1babaaeda7d1656097e086dc
semicloud <semicloud@163.com>
2019-05-28 09:09:08 +0800

    MySQL JDBC connection established

--- src/main/java/cn/iscas/PredictNowCountTotalCount.java
+++ src/main/java/cn/iscas/PredictNowCountTotalCount.java
@@ -1,18 +1,37 @@
 package cn.iscas;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class PredictNowCountTotalCount extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.println("Ok");
-        out.flush();
+        ServletConfig config = getServletConfig();
+        String url = config.getInitParameter("db.url");
+        System.out.println(url);
+        String username = config.getInitParameter("db.username");
+        System.out.println(username);
+        String password = config.getInitParameter("db.password");
+        System.out.println(password);
+        System.out.println("Connecting database...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            out.println("Database connected!");
+            out.flush();
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 
     @Override
